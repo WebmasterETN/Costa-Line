8a758b65ab39be580a4c8c387a8a82a5b63dfe0c
Jose Leonardo Gonzalez Valadez <jleonardo18071999@gmail.com>
2025-06-26 17:51:57 -0600

    feature: landing tecpan

--- src/layout/index.js
+++ src/layout/index.js
@@ -27,6 +27,7 @@ import "./layout-boletos-de-autobus-cuernavaca.js?v=1.0.2";
 import "./layout-boletos-de-autobus-taxco.js?v=1.0.2";
 import "./layout-boletos-de-autobus-ixtapa-zihuatanejo.js?v=1.0.2";
 import "./layout-boletos-de-autobus-iguala.js?v=1.0.2";
+import "./layout-boletos-de-autobus-tecpan.js?v=1.0.2";
 
 import "./layout-preguntas-frecuentes.js?v=1.0.2";
 
