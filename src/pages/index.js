0ba95e30148de14d260300a8e818f6a9952e1ee4
webmaster.etn <webmaster.etn@gmail.com>
2026-01-20 18:12:21 -0600

    ajustes-widget

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 /*-------------PAGINAS PRINCIPALES -------------------------*/
 
-import "./home/page-home.js?v=1.0.2";
+import "./home/page-home.js?v=1.0.3";
 import "./page-site-map.js?v=1.0.2";
 import "./page-404.js?v=1.0.2";
 
